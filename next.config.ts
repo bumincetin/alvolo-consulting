4ef2b6ba9d46a6aceacce0124013985945546470
bumincetin <cetinbumink@gmail.com>
2025-05-28 00:01:43 +0200

    feat: Configure project for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? '/cetin-aslan' : undefined,
+  assetPrefix: isProd ? '/cetin-aslan/' : undefined,
+  images: {
+    unoptimized: true, // Disable Next.js Image Optimization for static export
+  },
+  // trailingSlash: true, // Optional: if you prefer URLs with trailing slashes
 };
 
 export default nextConfig;
