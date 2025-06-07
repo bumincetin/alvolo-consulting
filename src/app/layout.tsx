f52f058a7e25c5816c1c5e81f2b99f96b3ba249d
bumincetin <cetinbumink@gmail.com>
2025-06-07 18:27:17 +0200

    cookies added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 // LanguageProvider is a client component, useLanguage is also for client components
 import { LanguageProvider } from "@/contexts/LanguageContext"; 
+import { CookieConsent } from "@/components/CookieConsent";
+import { ScriptLoader } from "@/components/ScriptLoader";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -19,9 +21,9 @@ const lato = Lato({
 
 // Metadata can now be exported as this is a Server Component by default
 export const metadata: Metadata = {
-  title: "Alvolo Consulting Danismanlik", // Default to TR, or consider removing if it should be truly dynamic via other means
+  title: "Alvolo Consulting Danışmanlık", // Default to TR, or consider removing if it should be truly dynamic via other means
   description: "Eğitim, bürokrasi, emlak ve adaptasyon süreçlerinizde profesyonel danışmanlık hizmetleri",
-  keywords: "Alvolo Consulting Danismanlik, Eğitim, bürokrasi, emlak, adaptasyon, danışmanlık",
+  keywords: "Alvolo Consulting Danışmanlık, Eğitim, bürokrasi, emlak, adaptasyon, danışmanlık",
   icons: {
     icon: '/ICON.png', // Points to public/icon.png
   },
@@ -48,6 +50,24 @@ export default function RootLayout({
         >
           <LanguageProvider> {/* LanguageProvider is a Client Component and can wrap children here */}
             {children}
+            <CookieConsent />
+            {/* Google Tag Manager (inline init, only after analytics consent) */}
+            <ScriptLoader
+              code={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PWK23TBJ');`}
+              category="analytics"
+              id="gtm-inline"
+            />
+            {/* Google Tag Manager (external script, only after analytics consent) */}
+            <ScriptLoader
+              src="https://www.googletagmanager.com/gtm.js?id=GTM-PWK23TBJ"
+              category="analytics"
+              async
+              id="gtm-external"
+            />
           </LanguageProvider>
         </ThemeProvider>
       </body>
